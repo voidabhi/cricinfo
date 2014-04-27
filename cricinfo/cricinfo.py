7fcc52e6cb72a857d2313e56693926d01836d4ab
Abhijeet <void.aby@gmail.com>
2014-04-27 17:30:50 +0530

    added .gitignore and cricinfo module

---
+++ cricinfo/cricinfo.py
@@ -0,0 +1,15 @@
+
+
+class Match(object):
+	"""
+	This class represents a cricket match
+	"""
+    def __init__(self):
+        # teams 
+        self.teams = []
+		# date of match
+		self.date = ''
+		# venue of match
+		self.venue = ''
+		
+	
