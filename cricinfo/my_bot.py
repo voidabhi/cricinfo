201c4bdb2bc9530ad7ef1ec1da18ceda936c1e23
Abhijeet Mohan <void.aby@gmail.com>
2015-06-27 22:10:54 +0530

    Update my_bot.py

--- cricinfo/my_bot.py
+++ cricinfo/my_bot.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 r = requests.get('http://static.cricinfo.com/rss/livescores.xml')
 soup = BeautifulSoup(r.text)
-matches = soup.find_all('title')
+matches = soup.find_all('item')
 
 for match in matches:
 	print match.contents['title']
