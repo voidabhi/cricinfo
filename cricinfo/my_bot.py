49ac8756f89fce52e74ee4b78a42ff89d01eb99c
Abhijeet Mohan <void.aby@gmail.com>
2015-09-03 22:32:33 +0530

    modularizing

--- cricinfo/my_bot.py
+++ cricinfo/my_bot.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup
 
 CRICINFO_RSS_URL = 'http://static.cricinfo.com/rss/livescores.xml'
 
-r = requests.get(CRICINFO_RSS_URL)
-soup = BeautifulSoup(r.text)
-matches = soup.find_all('item')
-
-print matches
-
+def get_matches():
+	r = requests.get(CRICINFO_RSS_URL)
+	soup = BeautifulSoup(r.text)
+	return soup.find_all('item')
+	
+matches = get_matches()
 for match in matches:
 	print match.contents['title']
 	print match.contents['description']
