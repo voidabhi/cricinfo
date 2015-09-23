3ccfed2e70e6da68452d466353c7b0df1ff9811c
Abhijeet <void.aby@gmail.com>
2015-09-24 01:51:19 +0530

    Add fetching live scores feature

--- cricinfo/my_bot.py
+++ cricinfo/my_bot.py
@@ -2,17 +2,51 @@
 
 import requests
 from bs4 import BeautifulSoup
+import xmltodict
+import click
 
 CRICINFO_RSS_URL = 'http://static.cricinfo.com/rss/livescores.xml'
 
-# Fetching matches
+class Match(object):
+
+	def __init__(self, title, link, description, guid):
+		self.title = title
+		self.link = link
+		self.description = description
+		self.guid = guid
+	
+	@staticmethod
+	def from_xml(xml):
+		item = xmltodict.parse(xml)['item']
+		return Match(item['title'], item['link'], item['description'], item['guid'])
+		
+	def __repr__(self):
+		return self.title
+
 def get_matches():
+	"""Fetches matches from the cricinfo url"""
+	
 	r = requests.get(CRICINFO_RSS_URL)
 	soup = BeautifulSoup(r.text)
-	return soup.find_all('item')
+	for match in soup.find_all('item'):
+		yield Match.from_xml(str(match))
 	
-matches = get_matches()
-for match in matches:
-	print match.contents['title']
-	print match.contents['description']
-	print match.contents['link']
+def print_posts(matches):
+  """Prints all matches to the console."""
+
+  click.echo()
+  for match in matches:
+    click.secho('%s\t' % match.title, bold=True, fg="red", nl=False)
+    click.echo()
+
+@click.command()
+def main():
+  """A cli to Cricinfo to see live scores"""
+  
+  # fetch matches
+  matches = get_matches()
+  # print matches
+  print_posts(matches)
+
+if __name__ == '__main__':
+  main()
