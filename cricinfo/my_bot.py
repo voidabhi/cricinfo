e3ec75571ca0907ca228267295d09861851dcce2
Abhijeet Mohan <void.aby@gmail.com>
2016-05-01 23:42:41 +0530

    fix indentation

--- cricinfo/my_bot.py
+++ cricinfo/my_bot.py
@@ -9,27 +9,27 @@ from ConfigParser import SafeConfigParser
 
 
 def get_config(key):
-	""" Fetch config from config file """
-	parser = SafeConfigParser()
-	parser.read('.config')
-	return parser.get(key)
+  """ Fetch config from config file """
+  parser = SafeConfigParser()
+  parser.read('.config')
+  return parser.get(key)
 
 class Match(object):
-	""" Represents a cricinfo match """
-	
-	def __init__(self, title, link, description, guid):
-		self.title = title
-		self.link = link
-		self.description = description
-		self.guid = guid
-	
-	@classmethod
-	def from_xml(self, xml):
-		item = xmltodict.parse(xml)['item']
-		return Match(item['title'], item['link'], item['description'], item['guid'])
-		
-	def __repr__(self):
-		return '<Match=%s>'%self.title
+  """ Represents a cricinfo match """
+  
+  def __init__(self, title, link, description, guid):
+      self.title = title
+      self.link = link
+      self.description = description
+      self.guid = guid
+  
+  @classmethod
+  def from_xml(self, xml):
+      item = xmltodict.parse(xml)['item']
+      return Match(item['title'], item['link'], item['description'], item['guid'])
+      
+  def __repr__(self):
+      return '<Match=%s>'%self.title
 
 def get_matches():
    """Fetches matches from the cricinfo url"""
@@ -37,8 +37,8 @@ def get_matches():
    r = requests.get(parser.get('url'))
    soup = BeautifulSoup(r.text)
    for match in soup.find_all('item'):
-	yield Match.from_xml(str(match))
-	
+    yield Match.from_xml(str(match))
+    
 def print_matches(matches):
   """Prints all matches to the console."""
 
